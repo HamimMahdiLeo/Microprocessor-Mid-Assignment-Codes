e352b5cf790d081a2bc892728486496ba03f85a1
HamimMahdiLeo <23-52828-2@student.aiub.edu>
2026-07-19 17:32:46 +0600

    4a using ID 23-52828-2

---
+++ 4a/4a.ino
@@ -0,0 +1,21 @@
+#define LED_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+
+  cli();
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); // CTC mode, prescaler 1024
+  OCR1A = 9094; // for 582ms
+  TIMSK1 = (1 << OCIE1A); // enable compare match interrupt
+  TCNT1 = 0;
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect) {
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // toggle every 582ms
+}
+
+void loop() {
+  // main loop unused; toggling handled by ISR
+}
